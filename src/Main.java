a9c0fd144c1281808a0dbf95107e932987957100
Sonia <soni.singh.2k11@gmail.com>
2024-03-31 01:26:58 +0530

    created add method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        System.out.println("Adding x and y = " + Cal.add(10,90));
     }
 }
